a59a849a190dfd4cc7d8c7f8863b269a13f91c15
meoki <1084415961@qq.com>
2021-03-12 22:38:12 +0800

    准备开始

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Layout, Menu } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import S from './Layout.module.less'
+
+const { Header, Sider, Content } = Layout;
+
+class SiderDemo extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Sider className={S.trigger} trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className={S.logo} />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className={S.sitelayout}>
+          <Header className={S.sitelayoutbackground} style={{ padding: 0 }}>
+            {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: this.toggle,
+            })}
+          </Header>
+          {/* 中心部分 */}
+          <Content
+            className={S.sitelayoutbackground}
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+// ReactDOM.render(<SiderDemo />, mountNode);
+export default (SiderDemo)
